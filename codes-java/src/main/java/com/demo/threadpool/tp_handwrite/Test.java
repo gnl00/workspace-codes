12fd8bc6d7e9d17e10002a2f1331674a670227cd
gnl00 <gnl0@qq.com>
2025-06-06 15:10:51 +0800

    update

--- codes-java/src/main/java/com/demo/threadpool/tp_handwrite/Test.java
+++ codes-java/src/main/java/com/demo/threadpool/tp_handwrite/Test.java
@@ -4,6 +4,9 @@ import com.demo.threadpool.tp_handwrite.core.AThreadPool;
 import com.demo.threadpool.tp_handwrite.core.RunnableWrapper;
 import com.demo.threadpool.tp_handwrite.reject.DiscardPolicy;
 
+/**
+ * @see <a href="https://ik3te1knhq.feishu.cn/wiki/RBA0wY3EqipCsvkThWIc2EGlnjh">手写线程池</a>
+ */
 public class Test {
     public static void main(String[] args) throws InterruptedException {
         AThreadPool threadPool = new AThreadPool(1, 4, 2, 3, 2000L, new DiscardPolicy());
